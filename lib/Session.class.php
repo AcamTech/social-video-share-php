84d89ab1bc9e69021be7e569b74be141f6228209
Gabe Lalasava <gabe@fijiwebdesign.com>
2018-01-20 16:21:46 +0800

    Added namespacing to Session lib

--- lib/Session.class.php
+++ lib/Session.class.php
@@ -12,17 +12,31 @@ namespace TorCDN;
  * $Session->get('foo'); // bar
  */
 class Session {
+
+	const DEFAULT_SESSION_NS = 'TorCDN';
 	
 	/**
-	 * @var {String} Session ID
+	 * @var String Session ID
 	 */
 	protected $id;
 
 	/**
-	 * Construct the session, start if not started
-	 * @param {String} Session ID
+	 * @var String Session namespace
 	 */
-	public function __construct($id = null) {
+	protected $namespace;
+
+	/**
+	 * @var Array Session data store
+	 */
+	protected $SESSION = [];
+
+	/**
+	 * Construct the session, start if not started
+	 * @param String Namespace
+	 * @param String Session ID
+	 */
+	public function __construct($ns = null, $id = null) {
+		$this->setNamespace($ns ? $ns : self::DEFAULT_SESSION_NS);
 		if (session_status() != PHP_SESSION_ACTIVE) {
 			if ($id) {
 				session_id($id);
@@ -35,46 +49,72 @@ class Session {
 				session_start();
 			}
 		}
+		$this->SESSION = &$_SESSION[$this->getNamespace()];
+	}
+
+	/**
+	 * Persist the session before process ends
+	 */
+	public function __destruct() {
+		$_SESSION[$this->getNamespace()] = $this->SESSION;
 	}
 
 	/**
 	 * Retrieve a session value
-	 * @param {String} Namespace
-	 * @param {Mixed} Default value to return if namespace is not set
+	 * @param String Namespace
+	 * @param Mixed Default value to return if namespace is not set
 	 */
 	public function get($name, $default = null) {
-		return isset($_SESSION[$name]) ? $_SESSION[$name] : $default;
+		return isset($this->SESSION[$name]) ? $this->SESSION[$name] : $default;
 	}
 
 	/**
 	 * Set a session value
-	 * @param {String} Namespace
-	 * @param {String} Value
+	 * @param String Namespace
+	 * @param String Value
 	 */
 	public function set($name, $value) {
-		return $_SESSION[$name] = $value;
+		return $this->SESSION[$name] = $value;
 	}
 
 	/**
 	 * Check if a session value has been set
-	 * @param {String} Namespace
+	 * @param String Namespace
 	 */
 	public function isset($name) {
-		return  isset($_SESSION[$name]);
+		return  isset($this->SESSION[$name]);
 	}
 
 	/**
 	 * Unset a session value
-	 * @param {String} Namespace
+	 * @param String Namespace
 	 */
 	public function unset($name) {
-		unset($_SESSION[$name]);
+		unset($this->SESSION[$name]);
 	}
 
 	/**
 	 * Destroy the session
 	 */
 	public function destroy() {
-		session_destroy();
+		$this->SESSION = [];
 	}
+
+	/**
+	 * Set a namespace to save session data to
+	 * @param String $namespace
+	 * @return void
+	 */
+	public function setNamespace($namespace) {
+		$this->namespace = $namespace;
+	}
+
+	/**
+	 * Get the session namespace
+	 * @return String
+	 */
+	public function getNamespace() {
+		return $this->namespace ?: self::DEFAULT_SESSION_NS;
+	}
+	
 }

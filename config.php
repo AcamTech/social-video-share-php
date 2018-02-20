3eaa8b2126e7f0e20253db41984f2400d89d3c19
Gabe Lalasava <gabe@fijiwebdesign.com>
2018-02-21 04:48:06 +0800

    Latakoo official config

--- config.php
+++ config.php
@@ -9,36 +9,37 @@ $scriptUri = strtok($request->getUri(), '?');
 
 $config = [
   'google' => [
-    "app_name" => "Latakoo",
+    "app_name" => "Delivered by Latakoo",
     "response_type" => "code",
-    "client_id" => "850095945888-2hges0en1jud5genpgt01hfnq3b6ord5.apps.googleusercontent.com",
-    "client_secret" => "Qlavzeo3LyF98fnQeVXBnStE",
+    "client_id" => "287901360233-4dg5thvjnc0svt22gk4crtnv4p9icu0c.apps.googleusercontent.com",
+    "client_secret" => "0D4xmzBmB0Cd6ExYQPZ0lDBA",
     "redirect_uri" => $scriptUri,
       "scope" => [
           'https://www.googleapis.com/auth/youtube.force-ssl'
       ]
     ],
   's3' => [
-    'region' => 'ap-southeast-1',
+    'region' => 'us-east-1',
+    'bucket' => 'pftp-data',
     'credentials' => [
-        'key' => 'AKIAI4LS43SGBDJDZ4RQ',
-        'secret' => '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'
+        'key' => 'AKIAIZVH4RMOI6TEYJSA',
+        'secret' => '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'
     ]
   ],
   'vimeo' => [
-    'client_id' => '4bb96180119c6d1e212922708c2de9f7fac761d6',
-    'client_secret' => 'IiYRQQzf78mnUkXtvb50SnZs/ema7J/HIVNekpu+4mSZpAmojkif0zW9A+g4rrHBCpduTpdCbaXngIMBRCiqcI/3hBYmXZHVdo8Ro67SWIFGx9xBdsuRd4LuN/wsVx8A',
+    'client_id' => '665925df8ac28a1d0308da138f7318382a9c0974',
+    'client_secret' => 'O+7wWD5NytBRoFcKDUZ57HWq0bqkNlI3XCbPp3nc1wtuve+dNIIZqcOEN3DbjoseUZ5fHNDts4UmuHrooLlkrdTsbV/FrU0maBxNyxOymfoWA2AGtMcZMml5RkDIGKBB',
     'scopes' => ['create', 'upload'], // https://developer.vimeo.com/api/authentication#supported-scope
     'redirect_uri' => $scriptUri
   ],
   'twitter' => [
-    'key' => '7KJdqkXKVJvUTUMB2oGP8WwzL',
-    'secret' => 'sG6LxfJfc7afQGkBS2H550htOXwjttATf38MGUeZJ7n5lF1A7T',
+    'key' => 'pNMGbGGhPIlfqQ4o3n151KwI0',
+    'secret' => 'D04R2fhyE0gccFzkApqA3rPKIuoa6EAKVqK546lsBR8dRsM9qO',
     'callback_url' => $scriptUri
   ],
   'facebook' => [
-    'app_id' => '552377038443474', // Replace {app-id} with your app id
-	  'app_secret' => '3d3a54b33ff57cbadd165e1d5b09697c',
+    'app_id' => '248990911853204',
+	  'app_secret' => 'd6f8aa2d88bfe629a32a2264dcc22872',
     'default_graph_version' => 'v2.3',
     //'default_access_token' => null, // optional
     'permissions' => [
